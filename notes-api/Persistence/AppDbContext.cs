37e8465a2820ccfef48c441d1878227991e431de
Shazni Shiraz <shaznishiraz+github@gmail.com>
2024-06-15 17:02:39 +0530

    Create AppDbContext.cs

---
+++ notes-api/Persistence/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using notes_api.Models;
+
+namespace notes_api.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
